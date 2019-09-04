d102b17386a9748beee872615741cdd333a51f00
jdotaz <50921075+jdotaz@users.noreply.github.com>
2019-09-04 00:38:27 -0500

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int binario;
+  cin >> binario;
+  int descomp=binario;
+  int digitos=0;
+  while (descomp>0){
+    descomp=descomp/10;
+    digitos=digitos+1;
+  }
+  digitos=digitos-1;
+  //////////////////////////////
+  int divisor=1;
+  for(int i=digitos;i>0;i--){
+    divisor=divisor*10;
+  }
+
+  //////////////////////////////
+  int total=0;
+  while (digitos>-1){
+    if (binario/divisor==1){
+      int potdos=1;
+      for(int i=digitos;i>0;i--){
+        potdos=potdos*2;
+      }
+      total=total+potdos;
+      binario=binario-divisor;
+    }
+    divisor=divisor/10;
+    digitos=digitos-1;
+  }
+  cout << total << endl;
+
+  int decimal;
+  int total2=0;
+  int mult=1;
+  cin >> decimal;
+  while (decimal>0){
+    total2=total2+mult*(decimal%2);
+    decimal=(decimal/2);
+    mult=mult*10;
+  }
+  cout << total2<<endl;
+}
