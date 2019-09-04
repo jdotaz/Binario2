9db06ad0a6ab4d5805a83af8c15cd48442c5c15a
jdotaz <50921075+jdotaz@users.noreply.github.com>
2019-09-04 00:39:23 -0500

    Create decimal.cpp

---
+++ decimal.cpp
@@ -0,0 +1,11 @@
+  int decimal;
+  int total2=0;
+  int mult=1;
+  cin >> decimal;
+  while (decimal>0){
+    total2=total2+mult*(decimal%2);
+    decimal=(decimal/2);
+    mult=mult*10;
+  }
+  cout << total2<<endl;
+}
