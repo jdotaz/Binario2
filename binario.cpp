1cb8d8ed46761fccbbc5d24539178833d4fd8414
jdotaz <50921075+jdotaz@users.noreply.github.com>
2019-09-04 00:39:00 -0500

    Update and rename README.cpp to binario.cpp

--- binario.cpp
+++ binario.cpp
@@ -35,14 +35,3 @@ int main()
   }
   cout << total << endl;
 
-  int decimal;
-  int total2=0;
-  int mult=1;
-  cin >> decimal;
-  while (decimal>0){
-    total2=total2+mult*(decimal%2);
-    decimal=(decimal/2);
-    mult=mult*10;
-  }
-  cout << total2<<endl;
-}
